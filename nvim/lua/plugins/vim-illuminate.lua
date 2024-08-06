e746cca68ac8da420cef0d07d3f5e6ecce54b035
MID3EE-ARCH <169875752+MID3EE-ARCH@users.noreply.github.com>
2024-08-06 21:37:15 +0530

    Updated dotfiles

--- nvim/lua/plugins/vim-illuminate.lua
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -3,5 +3,9 @@ return {
 	lazy = false,
 	config = function()
 		require("illuminate").configure({})
+		-- Set custom highlight groups
+		vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#3e4451", underline = true })
+		vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#3e4451", underline = true })
+		vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#3e4451", underline = true })
 	end,
 }
